163b7b1f6a5db8867ca83b547e0f570f840e8558
Arnesto <arnestoarnesto94@gmail.com>
2025-04-28 23:36:43 +0200

    Commit @23:36

---
+++ Script/amount.js
@@ -0,0 +1,66 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// price = 12.55 ;
+
+
+// function amountChange(clickedElement, amount) {
+//      let container = clickedElement.closest('.amount');
+//      let amountNumber = container.querySelector('.amountNumber');
+//      if (amountNumber) {
+//         value = parseInt(amountNumber.value);
+//         value += amount;
+//         if (value < 1) {value = 1};
+//         amountNumber.value = value
+//      }
+// }
+// function amountSUm(container,value,price) {
+//     let sumContainer = container.closest('.amount-sum');
+//     sum = sumContainer.querySelector('.sumNumber');
+//     sumValue = parseFloat(sum)*value*price;
+//     sum.innerHtml = sumValue;
+
+    
+// }
+
+
+
+
+
+
+
+// let amount = document.querySelector('.amount');
+// if (amount) {
+//     amount.addEventListener('click',function(event){
+//         let amountNumber = amount.querySelector('.amountNumber')
+//         if (event.target.classList.contains('amountPlus')) {
+//             amountNumber.value ++ ;
+            
+//         } else if (event.target.classList.contains('amountMinus')) {
+//             amountNumber.value -- ;
+//             if (amountNumber.value < 1) { amountNumber.value = 1;}
+//         }
+//         amountNumber.value = amountNumber.value;
+
+//     })
+    
+// }
